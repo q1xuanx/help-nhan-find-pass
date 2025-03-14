12018dcf67d847e4fbf72e14605034918fdfe984
nhan <nhoang2929@gmail.com>
2025-03-14 19:52:47 +0700

    add solutions.java

--- src/main/java/learn/Solutions.java
+++ src/main/java/learn/Solutions.java
@@ -5,7 +5,7 @@ import java.util.List;
 
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class Main {
+public class Solutions {
     public static void main(String[] args) {
         List<List<Integer>> result = new ArrayList<>();
         List<Integer> temp = new ArrayList<>();
